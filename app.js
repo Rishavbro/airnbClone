7c3940d22f3ea70a22b60b2a9396e6631ec6dfd4
Rishavbro <kumarrishavsingh125@gmail.com>
2024-05-26 00:45:42 +0530

    created new and create route

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const Listing = require("./models/listing.js");
 const path = require("path");
 
 app.set("view engine", "ejs");
-app.set("views",path.join(__dirname,"views"))
+app.set("views",path.join(__dirname,"views"));
+app.use(express.urlencoded({extended:true}))
 
 main().then(()=>{
     console.log("connected to db")
@@ -19,9 +20,28 @@ async function main() {
     // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
   }
 //INDEX ROUTE
-app.get("/listing",async (req,res)=>{
+app.get("/listings",async (req,res)=>{
     const allListing = await Listing.find({});
     res.render("./listings/index",{allListing})
 });
+
+//SHOW ROUTE
+app.get("/listing/:id", async (req,res)=>{
+    let {id} = req.params;
+    let listing = await Listing.findById(id);
+    res.render("./listings/show",{listing})
+});
+
+//POST ROUTE
+app.get("/listings/new",(req,res)=>{
+   res.render("./listings/new")
+});
+
+app.post("/listings",async (req,res)=>{
+   
+   const newListing = new Listing(req.body.listing);
+  await newListing.save()
+   res.redirect("/listings");
+})
 
 app.listen(8080);
